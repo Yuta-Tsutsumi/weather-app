0056c1ba7048164265ae95d2deba65252f1ec36b
堤佑太 <watagasi.789@gmail.com>
2021-12-30 23:35:56 +0900

    文字の位置の調整

--- src/components/molecules/Temperature/Temperature.tsx
+++ src/components/molecules/Temperature/Temperature.tsx
@@ -3,14 +3,16 @@ import styles from "./Temperature.module.scss";
 
 export const Temperature: React.FC = () => {
   return (
-    <div className={styles.aaa}>
+    <div className={styles.app}>
       <div>
-        <p>0月0日 現在時刻</p>
-        <h1>現在地</h1>
-        <h1>0℃</h1>
-        <p>体感温度:0℃ 最高気温:0℃ 最低気温:0℃</p>
-        <p>風:0m/s北寄りの風 気圧0hPa 湿度:0%</p>
-        <h2>1時間ごとの予測</h2>
+        <div className={styles.month}>0月0日 現在時刻</div>
+        <div className={styles.position}>現在地</div>
+        <div className={styles.zero}>0℃</div>
+        <div className={styles.temperature}>
+          体感温度:0℃ 最高気温:0℃ 最低気温:0℃
+        </div>
+        <div className={styles.wind}>風:0m/s北寄りの風 気圧0hPa 湿度:0%</div>
+        <div className={styles.time}>1時間ごとの予測</div>
       </div>
     </div>
   );

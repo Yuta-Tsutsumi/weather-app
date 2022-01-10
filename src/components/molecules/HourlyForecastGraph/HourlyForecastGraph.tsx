aa8812eb0b895093dbd016bce237c5add2dabe80
堤佑太 <watagasi.789@gmail.com>
2022-01-10 23:13:37 +0900

    気温グラフの追加

--- src/components/molecules/HourlyForecastGraph/HourlyForecastGraph.tsx
+++ src/components/molecules/HourlyForecastGraph/HourlyForecastGraph.tsx
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import styles from "./HourlyForecastGraph.module.scss";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "10時",
+    気温: 4000,
+  },
+  {
+    name: "11時",
+    気温: 3000,
+  },
+  {
+    name: "12時",
+    気温: 2000,
+  },
+  {
+    name: "13時",
+    気温: 2780,
+  },
+  {
+    name: "14時",
+    気温: 1890,
+  },
+  {
+    name: "15時",
+    気温: 2390,
+  },
+  {
+    name: "16時",
+    気温: 3490,
+  },
+];
 
 const HourlyForecastGraph: React.VFC = () => {
   return (
     <div>
       <div className={styles.time}>1時間ごとの予測</div>
-      <div className={styles.HourlyForecastGraph}></div>
+      <div className={styles.HourlyForecastGraph}>
+        <ResponsiveContainer width={500} height="100%">
+          <LineChart
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 20,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="name"
+              label={{ value: "時刻", position: "bottom" }}
+            />
+            <YAxis label={{ value: "気温", angle: -90, position: "left" }} />
+            <Tooltip />
+            <Legend verticalAlign="top" height={36} />
+            <Line type="monotone" dataKey="気温" stroke="#000" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };

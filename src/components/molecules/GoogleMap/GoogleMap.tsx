c22ba41ddeca219524412a883bc6a3eb86033df1
堤佑太 <watagasi.789@gmail.com>
2022-01-21 19:56:21 +0900

    リファクタリング

--- src/components/molecules/GoogleMap/GoogleMap.tsx
+++ src/components/molecules/GoogleMap/GoogleMap.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./GoogleMap.module.scss";
 
 const GoogleMap: React.VFC = () => {
-  return <div className={styles.root}>google mapをここに実装する</div>;
+  return <div className={styles.root}>google mapをここに実装する。</div>;
 };
 
 export default GoogleMap;

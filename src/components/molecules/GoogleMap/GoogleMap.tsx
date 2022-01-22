ce7b20eb3ce01af48eb0011bce4ddfbf6daae5f7
堤佑太 <watagasi.789@gmail.com>
2022-01-22 11:14:46 +0900

    GoogleMapの追加

--- src/components/molecules/GoogleMap/GoogleMap.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./GoogleMap.module.scss";
-
-const GoogleMap: React.VFC = () => {
-  return <div className={styles.root}>google mapをここに実装する。</div>;
-};
-
-export default GoogleMap;
--- src/components/pages/WeatherApp/WeatherApp.tsx
+++ src/components/pages/WeatherApp/WeatherApp.tsx
@@ -3,7 +3,7 @@ import TodayForecast from "../../molecules/TodayForecast/TodayForecast";
 import SearchForm from "../../molecules/SearchForm/SearchForm";
 import HourlyForecastGraph from "../../molecules/HourlyForecastGraph/HourlyForecastGraph";
 import WeeklyForecast from "../../molecules/WeeklyForecast/WeeklyForecast";
-import GoogleMap from "../../molecules/GoogleMap/GoogleMap";
+import GoogleMapComponents from "../../molecules/GoogleMapComponents/GoogleMapComponents";
 import styles from "./WeatherApp.module.scss";
 
 const WeatherApp: React.FC = () => {
@@ -14,7 +14,7 @@ const WeatherApp: React.FC = () => {
       </div>
       <div className={styles.middle}>
         <TodayForecast />
-        <GoogleMap />
+        <GoogleMapComponents />
       </div>
       <div className={styles.bottom}>
         <HourlyForecastGraph />

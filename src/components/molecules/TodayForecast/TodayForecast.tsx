986cf09a2bf53e5a6b29bdfa9692b261ae8c9d1c
堤佑太 <watagasi.789@gmail.com>
2022-01-29 10:31:47 +0900

    温度などのデータを入れる型の作成

--- src/components/molecules/TodayForecast/TodayForecast.tsx
+++ src/components/molecules/TodayForecast/TodayForecast.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./TodayForecast.module.scss";
 
+const number = 0;
 const TodayForecast: React.VFC = () => {
   return (
     <div className={styles.root}>
@@ -9,10 +10,16 @@ const TodayForecast: React.VFC = () => {
         <div className={styles.position}>現在地</div>
         <div className={styles.thermo}>0℃</div>
         <div className={styles.temperature}>
-          体感温度: 0℃　最高気温: 0℃　最低気温: 0℃
+          {/* 体感温度: 0℃　最高気温: 0℃　最低気温: 0℃ */}
+          <span>体感温度: {number}℃</span>
+          <span>最高温度: {number}℃</span>
+          <span>最低温度: {number}℃</span>
         </div>
         <div className={styles.wind}>
-          風: 0m/s 北寄りの風　気圧: 0hPa　湿度: 0%
+          {/* 風: 0m/s 北寄りの風　気圧: 0hPa　湿度: 0% */}
+          <span>風: {number}m/s 北寄りの風</span>
+          <span>気圧: {number}hPa</span>
+          <span>湿度 {number}%</span>
         </div>
       </div>
     </div>

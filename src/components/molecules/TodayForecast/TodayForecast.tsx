e7e47dabbba5aaf2f371cfa1f8cd36b6be1543da
堤佑太 <watagasi.789@gmail.com>
2022-01-29 11:01:23 +0900

    文字の間隔の変更

--- src/components/molecules/TodayForecast/TodayForecast.tsx
+++ src/components/molecules/TodayForecast/TodayForecast.tsx
@@ -9,16 +9,18 @@ const TodayForecast: React.VFC = () => {
         {number}月{number}日 現在時刻
       </div>
       <div className={styles.position}>現在地</div>
-      <div className={styles.thermo}>0℃</div>
-      <div className={styles.temperature}>
-        <span>体感温度: {number}℃</span>
-        <span>最高温度: {number}℃</span>
-        <span>最低温度: {number}℃</span>
-      </div>
-      <div className={styles.wind}>
-        <span>風: {number}m/s 北寄りの風</span>
-        <span>気圧: {number}hPa</span>
-        <span>湿度 {number}%</span>
+      <div className={styles.thermo}>{number}℃</div>
+      <div className={styles.roughTemperature}>
+        <div className={styles.temperature}>
+          <span className={styles.mainTemperature}>体感温度: {number}℃　</span>
+          <span className={styles.tempMax}>最高温度: {number}℃　</span>
+          <span className={styles.tempMin}>最低温度: {number}℃</span>
+        </div>
+        <div className={styles.wind}>
+          <span className={styles.mainWind}>風: {number}m/s 北寄りの風　</span>
+          <span className={styles.BarometricPressure}>気圧: {number}hPa　</span>
+          <span className={styles.humidity}>湿度 {number}%</span>
+        </div>
       </div>
     </div>
   );

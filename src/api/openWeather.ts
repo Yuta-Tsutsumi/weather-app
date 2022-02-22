9167ae581a7963f92852d70cbdb18a3f50f51e91
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-02-23 01:20:35 +0900

    コメントの追加

--- src/api/openWeather.ts
+++ src/api/openWeather.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
+//非同期通信を行う際は「async」をつける
 export const fetchTodayForecast = async () => {
   //処理が正常のときはtryの中身が処理される。（try catch(err)構文）
   try {
     const apiKey = "c6a862db0ad7ac1360b43d3dff990690";
     //awaitは非同期通信において処理が終わるまで次の行に行かないようにする記述。
-    //ブラウザ側からバックエンド側にデータをわたす処理を実装
+    //ブラウザ側からバックエンド側にジオコードのデータをわたす処理を実装
     const response = await axios.get(
       "https://api.openweathermap.org/data/2.5/onecall",
       {

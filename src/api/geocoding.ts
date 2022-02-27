313508572cbcd3e1fb5cadc1b10d7816e7f80a27
Yuta-Tsutsumi <watagasi.789@gmail.com>
2022-02-28 01:45:54 +0900

    コメントの追加

--- src/api/geocoding.ts
+++ src/api/geocoding.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 // Google Mapが提供している以下のエンドポイントを叩いて、住所から緯度/経度を取得する関数を作成する。
 // APIのドキュメントURL：https://developers.google.com/maps/documentation/geocoding/start
 // APIエンドポイント：https://maps.googleapis.com/maps/api/geocode/json
+
+// onSearchSubmitという名前で関数を定義し、引数はterm。非同期通信をしたい場合はasyncをつける事。
 export const geocoding = async (address: string) => {
   try {
     const endpoint = "https://maps.googleapis.com/maps/api/geocode/json";
